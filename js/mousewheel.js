62dff30e2da2f7229b6dc4638af8681d940a4904
betterwebproject <typographybandit@gmail.com>
2024-06-02 14:26:43 +0300

    Update mousewheel.js

--- js/mousewheel.js
+++ js/mousewheel.js
@@ -1,54 +1,27 @@
-// CSS for smooth scrolling
-document.documentElement.style.scrollBehavior = 'smooth';
-
-var isTouchDevice = 'ontouchstart' in document.documentElement;
-
-var scroll = function(e) {
-  var width = window.innerWidth;
-  if (width < 1024) return;
-
+var scroll = function( e ) {
   e.preventDefault();
-  if (Math.abs(e.deltaY) >= Math.abs(e.deltaX)) {
-    this.scrollLeft += (e.deltaY * 2);
+  if ( Math.abs( e.deltaY ) >= Math.abs( e.deltaX ) ) {
+    this.scrollLeft += ( e.deltaY * 2 );
   } else {
-    this.scrollLeft -= (e.deltaX * 2);
+    this.scrollLeft -= ( e.deltaX * 2 );
   }
-};
+}
 
-var startX, scrollLeft;
+var isTouchScreen = function() {
+  return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
+}
 
-var touchStart = function(e) {
+var applyScroll = function() {
   var width = window.innerWidth;
-  if (width < 1024) return;
+  if (isTouchScreen() && width >= 1024) {
+    document.documentElement.addEventListener('wheel', scroll);
+  } else {
+    document.documentElement.removeEventListener('wheel', scroll);
+  }
+}
 
-  startX = e.touches[0].pageX;
-  scrollLeft = this.scrollLeft;
-};
+// Initial check
+applyScroll();
 
-var touchMove = function(e) {
-  var width = window.innerWidth;
-  if (width < 1024) return;
-
-  var x = e.touches[0].pageX;
-  var walk = (startX - x) * 2; //scroll-fast
-  this.scrollLeft = scrollLeft + walk;
-  e.preventDefault();
-};
-
-// Using requestAnimationFrame for smoother scrolling
-var debounceScroll = function(fn) {
-  var frame;
-
-  return function(e) {
-    if (frame) {
-      cancelAnimationFrame(frame);
-    }
-    frame = requestAnimationFrame(function() {
-      fn(e);
-    });
-  };
-};
-
-document.documentElement.addEventListener('wheel', debounceScroll(scroll));
-document.documentElement.addEventListener('touchstart', touchStart);
-document.documentElement.addEventListener('touchmove', debounceScroll(touchMove));
+// Add event listener to reapply scroll behavior on resize
+window.addEventListener('resize', applyScroll);
