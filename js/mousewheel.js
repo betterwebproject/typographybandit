07d2283b3ac36036e736ca485373b40ac046212b
betterwebproject <typographybandit@gmail.com>
2024-06-02 14:32:22 +0300

    Update mousewheel.js

--- js/mousewheel.js
+++ js/mousewheel.js
@@ -1,27 +1,12 @@
 var scroll = function( e ) {
+  var width = window.innerWidth;
+  if(width <= 1024) return;
+      
   e.preventDefault();
   if ( Math.abs( e.deltaY ) >= Math.abs( e.deltaX ) ) {
-    this.scrollLeft += ( e.deltaY * 2 );
+      this.scrollLeft += ( e.deltaY * 2 );
   } else {
-    this.scrollLeft -= ( e.deltaX * 2 );
+      this.scrollLeft -= ( e.deltaX * 2 );
   }
 }
-
-var isTouchScreen = function() {
-  return ('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0);
-}
-
-var applyScroll = function() {
-  var width = window.innerWidth;
-  if (isTouchScreen() && width >= 1024) {
-    document.documentElement.addEventListener('wheel', scroll);
-  } else {
-    document.documentElement.removeEventListener('wheel', scroll);
-  }
-}
-
-// Initial check
-applyScroll();
-
-// Add event listener to reapply scroll behavior on resize
-window.addEventListener('resize', applyScroll);
+document.documentElement.addEventListener( 'wheel', scroll);
